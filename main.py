da1783e09629570765d7ca9debb2f4a87f0abcab
RHermanet14 <rhermanet14@gmail.com>
2026-02-25 02:34:33 -0500

    added frontend

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import yt_dlp
+import tkinter as tk
+from tkinter import ttk
 
 def download_mp3(url):
-    print("entered function")
     ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
@@ -14,5 +15,24 @@ def download_mp3(url):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-url = input("Enter url: ")
-download_mp3(url)
+def start_window():
+    root = tk.Tk()
+    root.title("Youtube to MP3")
+    root.geometry("500x300")
+
+    url = ttk.Entry(root, width=30)
+    url.pack(pady=10)
+
+    label = tk.Label(root, text="Enter in a valid Youtube URL.")
+    label.pack(pady=20)
+    
+    def on_button_click():
+        label.config(text=url.get())
+        # download_mp3(url)
+
+    button = tk.Button(root, text="Click Me", command=on_button_click)
+    button.pack(pady=10)
+
+    root.mainloop()
+
+start_window()
